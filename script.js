0462c5170ba80b54f30c2a7264b4864e06b3f22e
Victor <VictorCodrean@users.noreply.github.com>
2021-01-11 21:31:57 -0800

    generate password function done

--- script.js
+++ script.js
@@ -7,55 +7,30 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
+// Characters available
+var lowercase = "abcdefghijklmnopqrstuvwxyz";
+var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var number = "0123456789";
+var specialCharacter = "~!@#$%^&*()_-+={}[]<>?";
 
-// GIVEN I need a new, secure password
-// WHEN I click the button to generate a password
-// THEN I am presented with a series of prompts for password criteria
-// WHEN prompted for password criteria
-// THEN I select which criteria to include in the password
-// WHEN prompted for the length of the password
-// THEN I choose a length of at least 8 characters and no more than 128 characters
-// WHEN prompted for character types to include in the password
-// THEN I choose lowercase, uppercase, numeric, and/or special characters
-// WHEN I answer each prompt
-// THEN my input should be validated and at least one character type should be selected
-// WHEN all prompts are answered
-// THEN a password is generated that matches the selected criteria
-// WHEN the password is generated
-// THEN the password is either displayed in an alert or written to the page
 
-//Pick Random characters from the available elements
-function getRandomLower() {
-  var possibleLowercase = "abcdefghijklmnopqrstuvwxyz";
-  return possibleLowercase[Math.floor(Math.random() * possibleLowercase.length)];
-}
-
-function getRandomUpper() {
-  var possibleUppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  return possibleUppercase[Math.floor(Math.random() * possibleUppercase.length)];
-}
-
-function getRandomNumber() {
-  var possibleNumber = "0123456789";
-  return possibleNumber[Math.floor(Math.random() * possibleNumber.length)];
-}
-
-function getRandomSpecial() {
-  var possibleSpecialCharacter = "~!@#$%^&*()_-+={}[]<>?";
-  return possibleSpecialCharacter[Math.floor(Math.random() * possibleSpecialCharacter.length)];
+//---for my Understanding--- Math.floor(Math.random() * )
+function getRandom(ourcase) {
+  var index = Math.floor(Math.random() * ourcase.length);
+  var randomCharacter = ourcase[index];
+  return randomCharacter;
 }
 
 // Console log to check
-console.log(getRandomLower());
-console.log(getRandomUpper());
-console.log(getRandomNumber());
-console.log(getRandomSpecial());
+console.log(getRandom(lowercase));
+console.log(getRandom(uppercase));
+console.log(getRandom(number));
+console.log(getRandom(specialCharacter));
 
 // Function to get a valid number from user
 function getCharacterAmount() {
@@ -66,16 +41,11 @@ function getCharacterAmount() {
     charactersNo = parseInt(prompt("Provide a number for how many characters of generated PASSWORD \n!!! Make sure to choose one between 8-128")); 
   }
   return charactersNo;
-  //_____________How to return when user cancel it...___________
-  // if (charactersNo != null) {
-  //   alert("Alright, feel free to click -----Generate Password----- when ready.");
-  // } return;
-  
 }
 
 //Main function that will generate the PASSWORD
 function generatePassword() {
-  var passwordLength = getCharacterAmount();
+  var charactersNo = getCharacterAmount();
 
   var password = "";
 
@@ -93,60 +63,34 @@ function generatePassword() {
     withNumbers = confirm("         ----Numbers---- \nClick 'OK - for YES' and 'Cancel - for No'");
     withSymbols = confirm("          ----Symbols---- \nClick 'OK - for YES' and 'Cancel - for No'");
   }
+  // Available characters depending on situation
+  var availCharacters = "";
+  if (withLower) {
+    availCharacters += lowercase;
+    console.log(availCharacters);
+  }
 
+  if (withUpper) {
+    availCharacters += uppercase;
+    console.log(availCharacters);
+  }
 
-} 
+  if (withNumbers) {
+    availCharacters += number;
+    console.log(availCharacters);
+  }
 
+  if (withSymbols) {
+    availCharacters += specialCharacter;
+    console.log(availCharacters);
+  }
 
+  var password = "";
 
-
-
-
-
-
-
-
-
-
-
-
-
-// // 
-//   if (charactersNo === "") {
-//     alert(" ATTN \nMust be a number between 8-128.")
-//     getCharacterTypes();
-//   } else if (charactersNo) {
-//     var withLower = confirm("     ----lowercase---- \nDo you like your password to include  ---lowercase---   characters? \nClick 'OK - for YES' and 'Cancel - for No'");
-//     var withUpper = confirm("     ----UPPERCASE---- \nDo you like your password to include  ---uppercase---   characters? \nClick 'OK - for YES' and 'Cancel - for No'");
-//     var withNumbers = confirm("     ----Numbers---- \nDo you like your password to include  ---numeric---   characters? \nClick 'OK - for YES' and 'Cancel - for No'");
-//     var withSymbols = confirm("      ----Symbols---- \nDo you like your password to include  ---Symbols/Special characters---  ? \nClick 'OK - for YES' and 'Cancel - for No'");
+  for (var i = 0; i < charactersNo; i++) {
+    var randomIndex = Math.floor(Math.random() * availCharacters.length);
+    password += availCharacters[randomIndex];
+  }
   
-//   } else {
-//     alert("  Canceled/Wrong character: \n1. Try again by clicking 'GENERATE PASSWORD' \n2. Make sure to enter a number between: 8 - 128 next time.")
-//   }
-
-//   var passwordOptions = {
-//     lower: withLower,
-//     upper: withUpper,
-//     numbers: withNumbers,
-//     symbol: withSymbols,
-//     length: charactersNo,
-//   };
-//   return passwordOptions;
-
-
-// getCharacterTypes();
-
-
-// function generatePassword() {
-//    //initial variable values
-//    var password = "";
-//    var characterLength;
-//    var lowercase;
-//    var uppercase;
-//    var numeric;
-//    var specialCharacter;
- 
-
-
-// }
+  return password;
+} 
